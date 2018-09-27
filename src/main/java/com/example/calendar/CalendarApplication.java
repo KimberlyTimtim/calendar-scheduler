59905b588d7b10b791ecda1e807a0cedaeebc3a2
ARC-Kimberly Timtim <k.timtim@arcanys.com>
2018-09-27 23:55:49 +0800

    Minor changes

--- src/main/java/com/example/calendar/CalendarApplication.java
+++ src/main/java/com/example/calendar/CalendarApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalendarApplication {
 
-    private final static Logger LOGGER = LogManager.getLogger(CalendarApplication.class);
+//    private final static Logger LOGGER = LogManager.getLogger(CalendarApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CalendarApplication.class, args);
